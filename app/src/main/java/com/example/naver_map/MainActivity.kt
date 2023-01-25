8ba7a4a5bcb56e01ed13216bd159ae9c96a053fa
J  W <xmsxmsxpsxps@Naver.com>
2023-01-25 17:46:19 +0900

    api

--- app/src/main/java/com/example/naver_map/MainActivity.kt
+++ app/src/main/java/com/example/naver_map/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.naver_map
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.map.MapView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mapView = findViewById(R.id.mapView)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
